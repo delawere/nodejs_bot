9e73a22210e9b20d60d29c93c589fc5c0a35da0c
Dmitriy Zhiganov <delawere2009@yandex.ru>
2020-06-17 10:36:05 +0300

    listening port

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const getRate = ({ from, to }) => Rates.getRate(from, to);
 
 bot.start((ctx) => {
   const job = new CronJob(
-    '00 35 10 * * *',
+    '00 00 9 * * *',
     async () => {
       const { description, temp, feelsLike } = await getWeather();
       const { first_name: username } = ctx.from;
