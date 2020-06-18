738bad3d4db446a9f8b6e738f70c64d40765571b
Dmitriy Zhiganov <delawere2009@yandex.ru>
2020-06-18 09:05:33 +0300

    fix time

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const getRate = ({ from, to }) => Rates.getRate(from, to);
 
 bot.start((ctx) => {
   const job = new CronJob(
-    '00 00 9 * * *',
+    '00 10 09 * * *',
     async () => {
       const { description, temp, feelsLike } = await getWeather();
       const { first_name: username } = ctx.from;
